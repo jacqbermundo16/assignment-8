2a23fec883a5c93392ee82f4773fed9d82b7ffae
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-12-08 17:43:45 +0800

    ask for 3 numerical inputs

---
+++ lottery.py
@@ -0,0 +1,29 @@
+#Create a program that ask 3 numbers (0-9) from the user.
+#Generate 3 random winning numbers (0-9)
+#Display “Winner” if all 3 input numbers matched the generated numbers
+#Display ”You loss” if not
+#Display ”Try again y/n” after each game
+#If the user enter “y” the user will play again
+#if “n” the program will exit.
+
+#steps
+# display the name of the game and instructions
+print('LOTTERY')
+print(f' The program will ask you to input 3 numbers from 0-9.\n If you got all the numbers matched, you win.\n Results will be printed below.\n It will also ask if you want to play again.\n')
+#ask for 3 numerical inputs (0-9)
+def ask3Nums():
+    num1_ = int(input('Enter 1st number: '))
+    num2_ = int(input('Enter 2nd number: '))
+    num3_ = int(input('Enter 3rd number: '))
+    return num1_, num2_, num3_
+
+    
+#create a function that generates 3 random numbers(0-9) - save as winning numbers
+#evaluate the numbers; if all numbers matched display 'winner'
+#if not, display 'you loss'
+#aks the player after each game if they will play again (y/n are the options)
+# 'y' = another game
+# 'n' = exit game
+
+num1, num2, num3 = ask3Nums()
+print(num1,num2, num3)
