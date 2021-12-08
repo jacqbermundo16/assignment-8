6c930ee3fa5c1dffc81cb6f66a760fdbaa0f663d
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2021-12-08 20:32:13 +0800

    This program is a guessing game. It will generate a number between 0-100 and ask the user to guess it. It will give clues to lead the user. "Greater than" means the input is larger than the generated number, "Less than" means that the input is smaller. Once the user got the correct value, it will display the phrase "You guessed it right"

--- guess.py
+++ guess.py
@@ -7,14 +7,25 @@
 #steps
 #generate 1 random number from 0-100
 import random
+print('GUESS THE NUMBER\n The program will generate a random number from 0-100.\n It will give you clues to help you find the exact value.\n "Greater than" means that your input is larger.\n "Less than" means your input is smaller.\n')
 
 def guessNum():
     number = int(random.randint(0,100))
 #ask input 
     guess = int(input('Guess the number: '))
-    print(guess)
+    
 #evaluate the number, print 'Greater than' if input is greater than the number
-#print 'Less than' if the input is less than the number
+    while guess != number:
+        if guess > number:
+            print('Greater than')
+            guess = int(input('Guess the number: '))
+    #print 'Less than' if the input is less than the number
+        else:
+            if guess < number:
+                print('Less than')
+                guess = int(input('Guess the number: '))
 #create a loop until the input is equal to the generated number
+    print('You guessed it right')
+                
 
 guessNum()
